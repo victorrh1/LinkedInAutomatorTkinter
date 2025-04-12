c94d7077609709c25e729725f25be500c28706f0
J. Victor.py <55515144+victorrh1@users.noreply.github.com>
2025-04-12 09:57:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ import random
 
 logging.basicConfig(filename='erros.log', filemode='a', level=logging.ERROR)
 
-LINKEDIN_EMAIL = "Digite o usuário"
-LINKEDIN_SENHA = "Digite a senha"
-
 def conectar_com_pessoas_na_pagina_atual(driver, wait, window, conexoes_enviadas=0):
     limite_diario = 20
     try:
@@ -22,7 +19,7 @@ def conectar_com_pessoas_na_pagina_atual(driver, wait, window, conexoes_enviadas
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         sleep(2)
         
-        botoes_conectar = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(@aria-label, 'Convidar')]") ))
+        botoes_conectar = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(@aria-label, 'Convidar')]")))
         window.write_output('Botões de conectar encontrados\n')
         
         for botao_conectar in botoes_conectar:
@@ -65,15 +62,15 @@ def passar_para_proxima_pagina(driver, wait, window):
         window.write_output('Nenhuma próxima página encontrada ou erro ao avançar.\n')
         return False
 
-def fazer_login(driver, wait):
+def fazer_login(driver, wait, email, senha):
     driver.get("https://www.linkedin.com/login")
     sleep(3)
     
     campo_email = wait.until(EC.presence_of_element_located((By.ID, "username")))
-    campo_email.send_keys(LINKEDIN_EMAIL)
+    campo_email.send_keys(email)
     
     campo_senha = wait.until(EC.presence_of_element_located((By.ID, "password")))
-    campo_senha.send_keys(LINKEDIN_SENHA)
+    campo_senha.send_keys(senha)
     
     botao_login = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
     botao_login.click()
@@ -85,8 +82,6 @@ def iniciar_driver():
     for argument in arguments:
         chrome_options.add_argument(argument)
     
-    brave_path = "/usr/bin/brave-browser"
-    chrome_options.binary_location = brave_path
     driver = webdriver.Chrome(options=chrome_options)
     
     wait = WebDriverWait(driver, 10, poll_frequency=1, ignored_exceptions=[
@@ -97,9 +92,10 @@ def iniciar_driver():
     
     return driver, wait
 
-def iniciar_automacao(palavra_chave, window):
+def iniciar_automacao(palavra_chave, window, email, senha):
     driver, wait = iniciar_driver()
-    fazer_login(driver, wait)
+
+    fazer_login(driver, wait, email, senha)
     link_pesquisa_por_palavra_chave = f'https://www.linkedin.com/search/results/people/?keywords={palavra_chave}&origin=SWITCH_SEARCH_VERTICAL&sid=oJd'
     driver.get(link_pesquisa_por_palavra_chave)
     sleep(5)
@@ -110,4 +106,4 @@ def iniciar_automacao(palavra_chave, window):
         if not passar_para_proxima_pagina(driver, wait, window):
             break
     
-    window.write_output('Automação concluída!')
+    window.write_output('Conexões concluída!')
